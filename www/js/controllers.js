63434da955fb59f9fa47c056db176f71f01825db
Filipe Esperandio <filipesperandio@gmail.com>
2014-09-28 19:15:31 -0300

    flow control

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,53 @@
-var SlideController = function($scope, $stateParams, $state, Presentation) {
+var SlideController = function($scope, $stateParams, $state, Presentation, PresentationControl) {
   var total = 15;
   var localCurrentSlide = parseInt($stateParams.id);
 
   Presentation.$asObject().$bindTo($scope, 'presentation');
 
+
   $scope.$watch('presentation.current', function() {
-    if($scope.presentation){
-      $state.go('slides', { id: $scope.presentation.current });
+    if (PresentationControl.isFollowing()) {
+      console.log(' is following');
+      if($scope.presentation){
+        moveTo($scope.presentation.current);
+      }
     }
   });
 
+
   var next = function() {
-    var current = $scope.presentation.current;
-    $scope.presentation.current = current < total ? current + 1 : total;
-  }
+    var current = localCurrentSlide;
+    var slide = current < total ? current + 1 : total;
+    moveToAndUpdate(slide);
+  };
 
   var previous = function() {
-    var current = $scope.presentation.current;
-    $scope.presentation.current = current-1 ? current-1 : 1
+    var current = localCurrentSlide;
+    var slide = current-1 ? current-1 : 1
+    moveToAndUpdate(slide);
+  };
+
+  var moveToAndUpdate = function(slide) {
+    if(PresentationControl.isPresenter()) {
+      console.log(' updating remote current slide' );
+      Presentation.$set('current', slide);
+    }
+    moveTo(slide);
+  };
+
+  var moveTo = function(slide) {
+    console.log("moving to " + slide);
+    $state.go('slides', { id: slide });
+  };
+
+
+  var isPresenterTaken = function() {
+    var presenterTaken = $scope.presentation ? $scope.presentation.presenter : false;
+    return presenterTaken && !PresentationControl.isPresenter();
+  };
+
+  var togglePresenter = function() {
+    PresentationControl.togglePresenter($scope.presentation);
   };
 
   var content = "templates/deck/" + localCurrentSlide + ".html";
@@ -28,11 +58,16 @@ var SlideController = function($scope, $stateParams, $state, Presentation) {
     total: total,
     content : content,
     next : next,
-    previous : previous
-
+    previous : previous,
+    isFollowing : PresentationControl.isFollowing,
+    toggleFollow : PresentationControl.toggleFollow,
+    togglePresenter: togglePresenter,
+    isPresenterTaken : isPresenterTaken,
+    isPresenter : PresentationControl.isPresenter
   }
 }
 
 angular.module('starter.controllers', [ 'starter.services' ])
-  .controller('SlideController', SlideController)
+  .controller('SlideController', [ '$scope', '$stateParams', '$state', 
+              'Presentation', 'PresentationControl', SlideController ])
 
